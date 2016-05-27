3ebb3ebee44c871c226683eda10f6130db4a5083
Sebastián Lara <jhoansebastianlara@gmail.com>
2016-05-26 20:02:40 -0500

    Initial Commit

---
+++ Noti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noti
+//
+//  Created by Sebastián  Lara on 5/26/16.
+//  Copyright © 2016 Sebastián  Lara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
